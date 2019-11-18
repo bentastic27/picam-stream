cf2a3422e0f8333b84b732ff901cf935f43caf2c
Ben Healey <bentastic27@gmail.com>
2019-11-18 18:00:37 -0500

    initial commit for proxy container

---
+++ nginx-proxy/config-gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+from jinja2 import Template
+from kubernetes import client, config
+
+template = """
+server {
+  listen 80;
+  {% for pod in pod_list %}
+  location /{{ pod.metadata.name }} {
+    proxy_pass http://{{pod.status.pod_ip }}:8000/
+  }{% endfor %}
+}
+"""
+
+config.load_incluster_config()
+v1 = client.CoreV1Api()
+
+pod_list = v1.list_namespaced_pod("picam", label_selector="app=picam").items
+tm = Template(template)
+
+config_file = open("/etc/nginx/conf.d/default.conf", "w")
+config_file.write(tm.render(pod_list=pod_list))
+config_file.close()
---
+++ nginx-proxy/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python3 /config-gen.py
+nginx -g "daemon off;" &
+
+while true; do
+  sleep 60
+
+  OLDSUM=$(md5sum /etc/nginx/conf.d/default.conf | cut -f1 -d' ')
+  python3 /config-gen.py
+  NEWSUM=$(md5sum /etc/nginx/conf.d/default.conf | cut -f1 -d' ')
+
+  if [ "$OLDSUM" = "$NEWSUM" ]; then
+    echo "change detected, reloading"
+    nginx -s reload
+  fi
+done
