8a95812909ec93bd46d7e32f585d038eb1856c31
Ben Healey <bentastic27@gmail.com>
2019-11-18 20:29:54 -0500

    modified app to return ingress endpoints instead

--- webapp-image/app.py
+++ webapp-image/app.py
@@ -1,5 +1,13 @@
 from kubernetes import client, config
 from flask import Flask, render_template
+from os import environ
+
+if "INGRESS_HOST" not in environ:
+    print("INGRESS_HOST isn't set, set to your desired IP/hostname of ingress node")
+    exit()
+else:
+    ingress_host = environ.get("INGRESS_HOST")
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,7 +18,7 @@ def index():
 
     pod_list = v1.list_namespaced_pod("picam", label_selector="app=picam")
 
-    return render_template("index.html", pod_list=pod_list.items)
+    return render_template("index.html", pod_list=pod_list.items, ingress_host=ingress_host)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
