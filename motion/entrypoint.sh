28b20de204d3643860171bbb5e56fd2fec6b2819
Ben Healey <bentastic27@gmail.com>
2020-02-20 10:11:58 -0500

    first iteration of the entrypoint script. Fingers crossed.

--- motion/entrypoint.sh
+++ motion/entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 python3 /config-gen.py
-
 motion -c /etc/motion/motion.config
 
-### TODO enable auto reloading per 
-# http://lavrsen.dk/foswiki/bin/view/Motion/MotionGuideGettingItRunning#Signals_40sent_with_e.g._kill_command_41
+while true; do
+    sleep 60
+    OLDSUM=$(find /etc/motion/conf.d -type f -exec md5sum {} \; | sort -k 2 | md5sum | cut -f1 -d' ')
+    python3 /config-gen.py
+    NEWSUM=$(find /etc/motion/conf.d -type f -exec md5sum {} \; | sort -k 2 | md5sum | cut -f1 -d' ')
+
+    if [ "$OLDSUM" != "$NEWSUM" ]; then
+        echo "change detected, reloading"
+        kill -s SIGHUP $(cat /var/run/motion/motion.pid)
+    fi
+done
